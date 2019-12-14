2035e251fa46c5e433c3b9264389aebfafaf0a4e
Romain Fallet <rfallet@numee.fr>
2019-12-14 11:17:23 +0100

    Update windows_install.ps1

--- windows_install.ps1
+++ windows_install.ps1
@@ -1,5 +1,5 @@
 # Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 # Git
